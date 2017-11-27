e54c5a744f54a67ae85cd912be69e3f948684f9f
petercz1 <email.peter.carroll@gmail.com>
2017-11-27 11:15:44 +0800

    You will meet an important person who will help you advance professionally.

--- js/app.js
+++ js/app.js
@@ -6,7 +6,6 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/home');
 
     $stateProvider
-
         // HOME STATES AND NESTED VIEWS ========================================
         .state('home', {
             url: '/home',
