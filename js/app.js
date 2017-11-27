4a2f760d82b1feb207c6222d96c5f031a3dfe256
petercz1 <email.peter.carroll@gmail.com>
2017-11-27 11:44:57 +0800

    It is undignified for a woman to play servant to a man who is not hers. 		-- Spock, "Amok Time", stardate 3372.7

--- js/app.js
+++ js/app.js
@@ -30,7 +30,44 @@ routerApp.config(function ($stateProvider, $urlRouterProvider) {
 
     // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
     .state('about', {
-      // we'll get to this in a bit       
+        url: '/about',
+        views: {
+
+            // the main template will be placed here (relatively named)
+            '': { templateUrl: 'partial-about.html' },
+
+            // the child views will be defined here (absolutely named)
+            'columnOne@about': { template: 'Look I am a column!' },
+
+            // for column two, we'll define a separate controller 
+            'columnTwo@about': { 
+                templateUrl: 'table-data.html',
+                controller: 'scotchController'
+            }
+        }
+
     });
 
 });
+
+routerApp.controller('scotchController', function($scope) {
+    
+        $scope.message = 'test';
+    
+        $scope.scotches = [
+            {
+                name: 'Macallan 12',
+                price: 50
+            },
+            {
+                name: 'Chivas Regal Royal Salute',
+                price: 10000
+            },
+            {
+                name: 'Glenfiddich 1937',
+                price: 20000
+            }
+        ];
+    
+    });
+    
