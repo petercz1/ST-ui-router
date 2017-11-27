6ce935b1b57cb1c48e0cf595471e26fd6e18ffd6
petercz1 <email.peter.carroll@gmail.com>
2017-11-27 11:36:11 +0800

    Indomitable in retreat; invincible in advance; insufferable in victory. 		-- Winston Churchill, on General Montgomery

--- js/app.js
+++ js/app.js
@@ -1,21 +1,36 @@
 // app.js
 var routerApp = angular.module('routerApp', ['ui.router']);
 
-routerApp.config(function($stateProvider, $urlRouterProvider) {
+routerApp.config(function ($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/home');
+  $urlRouterProvider.otherwise('/home');
 
-    $stateProvider
+  $stateProvider
 
-        // HOME STATES AND NESTED VIEWS ========================================
-        .state('home', {
-            url: '/home',
-            templateUrl: 'partials/partial-home.html'
-        })
+    // HOME STATES AND NESTED VIEWS ========================================
+    .state('home', {
+      url: '/home',
+      templateUrl: 'partials/partial-home.html'
+    })
 
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-        .state('about', {
-            // we'll get to this in a bit       
-        });
+    // nested list with custom controller
+    .state('home.list', {
+      url: '/list',
+      templateUrl: 'partial-home-list.html',
+      controller: function ($scope) {
+        $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+      }
+    })
+
+    // nested list with just some random string data
+    .state('home.paragraph', {
+      url: '/paragraph',
+      template: 'I could sure use a drink right now.'
+    })
+
+    // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+    .state('about', {
+      // we'll get to this in a bit       
+    });
 
 });
